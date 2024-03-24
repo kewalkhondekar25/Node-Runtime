4051a0d2c83d8f34dd30f09edab0a081829eb659
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-24 23:18:04 +0530

    feat - req query added

--- Express_Server/index.js
+++ Express_Server/index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+const sum = (n) => {
+    let compute = 0;
+    for(let i = 0; i <= n; i++)
+    {   
+        compute += i;
+    }
+    return compute;
+}
+
 app.get('/', (req, res) => {
-    res.send('hello world')
+    const param = req.query.n;
+    const ans = sum(param);
+    res.send('hello world' + ans);
 });
 
 app.listen(port, () => {
