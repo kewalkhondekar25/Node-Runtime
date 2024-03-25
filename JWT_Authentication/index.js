07ce8a3e0eab9ce7cd048c9a3a06d6040fabd5da
keal khondekar <kewalkhondekar25@gmail.com>
2024-03-26 00:40:33 +0530

    feat - jwt implemented

---
+++ JWT_Authentication/index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const port = 8082;
+const jwt = require('jsonwebtoken');
+const jwtPassword = "iknowexactlywhereyouare";
+
+const app = express();
+app.use(express.json());
+
+const agent = [
+    {
+        name: 'cypher',
+        email: 'cypher@valorant.gg'
+    },
+    {
+        name: 'omen',
+        email: 'omen@valorant.gg'
+    }
+];
+
+
+const handleVerify = (name, email) => {
+    let isVerify = false;
+    for(let i = 0; i < agent.length; i++){
+        if(agent[i].name == name && agent[i].email == email){
+            isVerify = true;
+        }
+    }
+    return isVerify;
+}
+
+app.get('/', (req, res) => {
+    res.json({
+        msg: 'authentication running'
+    })
+})
+
+app.post('/signin', (req, res) => {
+    const agentName = req.body.name;
+    const agentEmail = req.body.email;
+
+    if(!handleVerify(agentName, agentEmail)){
+        return res.json({
+            msg: 'agent doesnt exist in database'
+        })
+    }
+    let token = jwt.sign({name: agentName}, jwtPassword);
+    res.json({
+        token
+    })
+})
+
+app.listen(port, () => {
+    console.log(`server is listening on port: ${port}...`);
+})
